2cd69616c4e39c1a134198ecc0f4a73d11b304f7
Paul Cherpitel <pcherpitel@montrium.com>
2019-02-08 13:53:50 -0500

    Add DNS zone support

---
+++ src/PoshArmDeployment/Public/ResourceProviders/Microsoft.Network/DnsZone/Add-ArmDnsZoneVirtualNetwork.ps1
@@ -0,0 +1,30 @@
+function Add-ArmDnsZoneVirtualNetwork {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    [OutputType("DNSZ")]
+    Param(
+        [PSTypeName("DNSZ")]
+        [Parameter(Mandatory, ValueFromPipeline)]
+        $DNSZ,
+        [string]
+        [ValidateSet("Registration", "Resolution")]
+        $IntegrationType = "Resolution",
+        [Parameter(Mandatory)]
+        [PSTypeName("VirtualNetwork")]
+        $VirtualNetwork
+    )
+
+    If ($PSCmdlet.ShouldProcess("Adding DNS zone virtual network integration")) {
+        $networkEntry = @{
+            id = $VirtualNetwork._ResourceId
+        }
+
+        If ($IntegrationType -eq "Resolution") {
+            $DNSZ.properties.resolutionVirtualNetworks += $networkEntry
+        }
+        else {
+            $DNSZ.properties.registrationVirtualNetworks += $networkEntry
+        }
+
+        return $DNSZ
+    }
+}
---
+++ src/PoshArmDeployment/Public/ResourceProviders/Microsoft.Network/DnsZone/New-ArmDnsZoneARecord.ps1
@@ -0,0 +1,43 @@
+function New-ArmDnsZoneARecord {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    [OutputType("DNSZARecord")]
+    Param(
+        [PSTypeName("DNSZ")]
+        [Parameter(Mandatory)]
+        $DnsZone,
+        [string]
+        [ValidatePattern('^(\[.*\]|)|([a-z0-9-_]{1,127})$')]
+        [Parameter(Mandatory, ValueFromPipeline)]
+        $Name,
+        [int]
+        $TTL = 3600,
+        [string[]]
+        [Parameter(Mandatory)]
+        $IpV4Addresses
+    )
+
+    If ($PSCmdlet.ShouldProcess("Adding DNS zone A record")) {
+        $dnsZoneName = $DnsZone.name
+        $aRecord = [PSCustomObject][ordered]@{
+            _ResourceId = New-ArmFunctionResourceId -ResourceType Microsoft.Network/dnszones/A -ResourceName1 $dnsZoneName -ResourceName2 $Name
+            PSTypeName  = "DNSZARecord"
+            type        = 'Microsoft.Network/dnszones/A'
+            name        = "[concat('$dnsZoneName', '/', '$Name')]"
+            apiVersion  = $DnsZone.apiVersion
+            properties  = @{
+                TTL      = $TTL
+                ARecords = @()
+            }
+            dependsOn   = @()
+        }
+
+        foreach ($IpV4Address in $IpV4Addresses) {
+            $aRecord.properties.ARecords += @{
+                ipv4Address = $IpV4Address
+            }
+        }
+
+        $aRecord.PSTypeNames.Add("ArmResource")
+        return $aRecord
+    }
+}
