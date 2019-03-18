001a7e7a189f28dfbbb7e41c57ce98f5a6a7bea5
Mehryar <apwbd.d@gmail.com>
2019-03-18 12:27:12 -0400

    Refactoring resource group name building (#14)
    * add resource-group name builder function

---
+++ src/PoshArmDeployment/Public/New-ArmResourceGroupName.ps1
@@ -0,0 +1,24 @@
+function New-ArmResourceGroupName {
+    [CmdletBinding()]
+    param(
+        [string]
+        $ProjectName = $script.projectName,
+        [string]
+        $EnvironmentCode = $script.environmentCode,
+        [string]
+        $Context = $script.context,
+        [string]
+        $Location = $script.location
+    )
+
+    $ResourceGroupNameParts = @(
+        $ProjectName
+        $EnvironmentCode
+        $Context
+        $Location
+    ) | Where-Object {$_}
+    $ResourceGroupName = [string]::Join('-', $ResourceGroupNameParts)
+    $ResourceGroupName = $ResourceGroupName.ToLowerInvariant()
+
+    return $ResourceGroupName
+}
