55f5c0dc40e5e156f3f5d11a0373a596902b587b
Mehryar <apwbd.d@gmail.com>
2019-04-01 13:51:17 -0400

    service fabric backup  (#18)
    * Add cmd enabling service fabric back up on cluster

---
+++ src/PoshArmDeployment/Public/ResourceProviders/Microsoft.ServiceFabric/Add-ArmServiceFabricBackupService.ps1
@@ -0,0 +1,28 @@
+function Add-ArmServiceFabricBackupService {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    Param(
+        [PSTypeName("ServiceFabricCluster")]
+        [Parameter(Mandatory, ValueFromPipeline)]
+        $ServiceFabricCluster,
+        [Parameter(Mandatory)]
+        [string]
+        $EncryptionCertificateThumbprint
+    )
+
+    Process {
+        If ($PSCmdlet.ShouldProcess("Adding Backup and Restore to service fabric cluster")) {
+            $ServiceFabricCluster.properties.addonFeatures += "BackupRestoreService"
+            $ServiceFabricCluster.properties.fabricSettings += @{
+                parameters = @(
+                    @{
+                        name  = "SecretEncryptionCertThumbprint"
+                        value = $EncryptionCertificateThumbprint
+                    }
+                )
+                name       = "BackupRestoreService"
+            }
+        }
+
+        return $ServiceFabricCluster
+    }
+}
