208a683519975d1e4cecce4549c64aa0ca6a27e8
Paul Cherpitel <pcherpitel@montrium.com>
2019-05-22 11:26:01 -0400

    Add support for DocumentDb

---
+++ src/PoshArmDeployment/Public/ResourceProviders/Microsoft.DocumentDB/New-ArmDocumentDbAccount.ps1
@@ -0,0 +1,59 @@
+function New-ArmDocumentDbAccount {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    [OutputType("DocumentDbAccount")]
+    Param(
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [ValidatePattern('^(\[.*\]|[a-zA-Z0-9-]{3,50})$')]
+        [string]
+        $Name,
+        [ValidateSet('GlobalDocumentDB', 'MongoDB', 'Parse')]
+        [string]
+        $Kind = 'GlobalDocumentDB',
+        [ValidateSet('EnableCassandra', 'EnableTable', 'EnableGremlin')]
+        [string]
+        $Capability,
+        [switch]
+        $EnableMultipleWriteLocations,
+        [switch]
+        $VirtualNetworkFilterEnabled,
+        [string]
+        [ValidatePattern('^(?:[0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,2}$')]
+        $IpRangeFilter = '',
+        [string]
+        $ApiVersion = '2016-03-31',
+        [string]
+        $Location = $script:Location,
+        [string]
+        $LocationName = $script:LocationName
+    )
+
+    If ($PSCmdlet.ShouldProcess("Creates a new Arm Document DB account")) {
+        $DocumentDBAccount = [PSCustomObject][ordered]@{
+            _ResourceId = $Name | New-ArmFunctionResourceId -ResourceType 'Microsoft.DocumentDb/databaseAccounts'
+            PSTypeName  = "DocumentDbAccount"
+            type        = 'Microsoft.DocumentDb/databaseAccounts'
+            name        = $Name
+            kind        = $Kind
+            apiVersion  = $ApiVersion
+            location    = $Location
+            properties  = @{
+                databaseAccountOfferType      = 'Standard'
+                locations                     = @(
+                    @{
+                        id               = "[concat($Name, '-', '$Location')]"
+                        failoverPriority = 0
+                        locationName     = $LocationName
+                    }
+                )
+                enableMultipleWriteLocations  = $EnableMultipleWriteLocations.ToBool()
+                isVirtualNetworkFilterEnabled = $VirtualNetworkFilterEnabled.ToBool()
+                ipRangeFilter                 = $IpRangeFilter
+                virtualNetworkRules           = @()
+            }
+            dependsOn   = @()
+        }
+
+        $DocumentDBAccount.PSTypeNames.Add("ArmResource")
+        return $DocumentDBAccount
+    }
+}
