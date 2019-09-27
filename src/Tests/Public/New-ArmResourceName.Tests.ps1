04007d0a34ebe63b3ace01dbdb4ab13a2874b8a2
Miguel Bernard <mbernard@montrium.com>
2019-09-27 14:55:32 -0400

    Add test for New-ArmResourceName

--- src/Tests/Public/New-ArmResourceName.Tests.ps1
+++ src/Tests/Public/New-ArmResourceName.Tests.ps1
@@ -3,11 +3,22 @@ Import-Module "$ScriptDir/../../PoshArmDeployment" -Force
 
 Describe "New-ArmResourceName" {
     It "Given a '<Name>', it returns '<Expected>'" -TestCases @(
-        @{ Name = "name1"; Expected = "[concat('name10', uniqueString('sa','name1'))]" }
+        @{ Name = "name1"; ResourceType = "Microsoft.Storage/storageAccounts"; Expected = "[concat('name10', uniqueString('sa','name1'))]" }
+        @{ Name = "name1"; ResourceType = "Microsoft.KeyVault/vaults"; Expected = "[concat('name1-', uniqueString('kv','name1'))]" }
     ){
-        param($Name, $Expected)
+        param($Name, $ResourceType, $Expected)
 
-        $name = New-ArmResourceName -ResourceName $Name -ResourceType "Microsoft.Storage/storageAccounts"
+        $name = New-ArmResourceName -ResourceName $Name -ResourceType $ResourceType
+        $name | Should Be $Expected
+    }
+
+    It "Given no name, then resource type is used" -TestCases @(
+        @{ ResourceType = "Microsoft.Storage/storageAccounts"; Expected = "[concat('sa0', uniqueString('sa','sa'))]" }
+        @{ ResourceType = "Microsoft.KeyVault/vaults"; Expected = "[concat('kv-', uniqueString('kv','kv'))]" }
+    ){
+        param($ResourceType, $Expected)
+
+        $name = New-ArmResourceName -ResourceType $ResourceType
         $name | Should Be $Expected
     }
 }
