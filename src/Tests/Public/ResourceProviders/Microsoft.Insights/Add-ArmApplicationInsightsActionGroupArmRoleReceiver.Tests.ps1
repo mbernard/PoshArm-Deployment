3e6bcf42e90c4d5720694f46a795cf96534e8d88
Mehryar Keshavarz <mkeshavarz@montrium.com>
2019-11-13 15:55:19 -0500

    add integration and more test for armrolereceiver

--- src/Tests/Public/ResourceProviders/Microsoft.Insights/Add-ArmApplicationInsightsActionGroupArmRoleReceiver.Tests.ps1
+++ src/Tests/Public/ResourceProviders/Microsoft.Insights/Add-ArmApplicationInsightsActionGroupArmRoleReceiver.Tests.ps1
@@ -7,41 +7,53 @@ InModuleScope PoshArmDeployment {
         $ResourceType = "Microsoft.Insights/actionGroups"
         BeforeEach {
             $ActionGroup = New-ArmResourceName $ResourceType `
-                | New-ArmApplicationInsightsActionGroup -ShortName 'SomeActionGroup'
+            | New-ArmApplicationInsightsActionGroup -ShortName 'SomeActionGroup'
             $Expected = New-ArmResourceName $ResourceType `
-                | New-ArmApplicationInsightsActionGroup -ShortName 'SomeActionGroup'
+            | New-ArmApplicationInsightsActionGroup -ShortName 'SomeActionGroup'
         }
 
         Context "Unit tests" {
             $ExpectedName = 'SomeArmRole'
             $ExpectedRoleId = 'SomeGuid'
 
-            It "Given valid 'ActionGroup', Role Id and Name, it returns '<Expected>'" -TestCases @(
+            It "Given valid 'ActionGroup', Role Id, Name and one or more ArmRoleReceivers it returns '<Expected>'" -TestCases @(
                 @{  
-                    Name = $ExpectedName
-                    RoleId = $ExpectedRoleId
+                    Name                     = $ExpectedName
+                    RoleId                   = $ExpectedRoleId
                     DisableCommonAlertSchema = $false
                 },
                 @{  
-                    Name = $ExpectedName
-                    RoleId = $ExpectedRoleId
+                    Name                     = $ExpectedName
+                    RoleId                   = $ExpectedRoleId
                     DisableCommonAlertSchema = $true
+                },
+                @{  
+                    Name                     = $ExpectedName
+                    RoleId                   = $ExpectedRoleId
+                    DisableCommonAlertSchema = $false
+                    NumberOfArmRoleReceivers = 3
                 }
             ) {
-                param($Name, $RoleId, $DisableCommonAlertSchema)
+                param($Name, $RoleId, $DisableCommonAlertSchema, $NumberOfArmRoleReceivers = 1)
                 
-                $Expected.properties.armRoleReceivers += @(
-                    @{
-                        name                 = $Name
-                        roleId               = $RoleId
-                        useCommonAlertSchema = $DisableCommonAlertSchema
-                    }
-                )
+                for ($i = 0; $i -lt $NumberOfArmRoleReceivers; $i++) {
+                    $Expected.properties.armRoleReceivers += @(
+                        @{
+                            name                 = $ExpectedName
+                            roleId               = $ExpectedRoleId
+                            useCommonAlertSchema = $DisableCommonAlertSchema
+                        }
+                    )
+                }
 
                 $actual = $ActionGroup | Add-ArmApplicationInsightsActionGroupArmRoleReceiver -Name $Name -RoleId $RoleId -DisableCommonAlertSchema:$DisableCommonAlertSchema
 
+                for ($i = 0; $i -lt ($NumberOfArmRoleReceivers - 1); $i++) {
+                    $actual = $actual | Add-ArmApplicationInsightsActionGroupArmRoleReceiver -Name $Name -RoleId $RoleId -DisableCommonAlertSchema:$DisableCommonAlertSchema
+                }
+
                 ($actual | ConvertTo-Json -Compress | ForEach-Object { [System.Text.RegularExpressions.Regex]::Unescape($_) }) `
-                    | Should -BeExactly ($Expected | ConvertTo-Json -Compress| ForEach-Object { [System.Text.RegularExpressions.Regex]::Unescape($_) })
+                | Should -BeExactly ($Expected | ConvertTo-Json -Compress | ForEach-Object { [System.Text.RegularExpressions.Regex]::Unescape($_) })
             }
 
 
@@ -50,22 +62,23 @@ InModuleScope PoshArmDeployment {
             $expectedRoleId = "someRoleId"
 
             It "Given invalid 'ActionGroup' type, it throws '<Expected>'" -TestCases @(
-                @{ ActionGroup = "ApplicationInsightsActionGroup"; Name= $expectedName; RoleId = $expectedRoleId; Expected = $expectedException }
-                @{ ActionGroup = [PSCustomObject]@{Name = "Value" }; Name= $expectedName; RoleId = $expectedRoleId;; Expected = $expectedException }
+                @{ ActionGroup = "ApplicationInsightsActionGroup"; Name = $expectedName; RoleId = $expectedRoleId; Expected = $expectedException }
+                @{ ActionGroup = [PSCustomObject]@{Name = "Value" }; Name = $expectedName; RoleId = $expectedRoleId; ; Expected = $expectedException }
             ) { param($ActionGroup, $Name, $RoleId, $Expected)
                 { Add-ArmApplicationInsightsActionGroupArmRoleReceiver -ActionGroup $ActionGroup -Name $Name -RoleId $RoleId } | Should -Throw -ErrorId $Expected
             }
 
-        # Context "Integration tests" {
-        #     It "Default" -Test {
-        #         Invoke-IntegrationTest -ArmResourcesScriptBlock `
-        #         {
-        #             New-ArmResourceName $ResourceType `
-        #             | New-ArmApplicationInsightsMetricAlert `
-        #             | Add-ArmApplicationInsightsMetricAlertCriteriaODataType `
-        #             | Add-ArmResource
-        #         }
-        #     }
+            Context "Integration tests" {
+                It "Default" -Test {
+                    Invoke-IntegrationTest -ArmResourcesScriptBlock `
+                    {
+                        New-ArmResourceName $ResourceType `
+                        | New-ArmApplicationInsightsActionGroup -ShortName 'SomeActionGroup' `
+                        | Add-ArmApplicationInsightsActionGroupArmRoleReceiver -Name 'SomeName' -RoleId 'SomeRoleId' -DisableCommonAlertSchema:$true `
+                        | Add-ArmResource
+                    }
+                }
+            }
         }
     }
 }
